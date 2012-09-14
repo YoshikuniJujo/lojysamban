5a31d6eaa34a77884b94de69a984b7b72fe0f6f8
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-09-14 17:08:17 +0900

    show answer

--- lojsamban2.hs
+++ lojsamban2.hs
@@ -7,18 +7,36 @@ import qualified Language.Lojban.Parser as P
 import System.Environment
 import Data.Maybe
 import Data.Either
+import Data.List
 
 main :: IO ()
 main = do
 	[fn] <- getArgs
 	src <- readFile fn
 	let	Right p = parse src
-		rules = map readSentence $ getSentences p
-	Left q <- (readSentenceFact . either (error "bad") id . parse) `fmap` getLine
-	print (rules :: [Rule String Atom])
+		rules = map (readSentence "rules") $ getSentences p
+	Left q <- (readSentenceFact "top" . either (error "bad") id . parse)
+		`fmap` getLine
+	let	answer = ask q rules
+--	print (rules :: [Rule String Atom])
 --	print (q :: Fact Scope Atom)
-	putStrLn ""
-	print $ ask q rules
+	putStrLn $ showAnswerAll answer
+
+showAnswerAll a = if null a then "nago'i" else
+	intercalate " .a " $ map showAnswer $ map (lookupMA . onlyTop) a
+
+lookupMA = map snd . filter ((Var "top" (KOhA "ma") `elem`) . fst)
+
+showAnswer as = if null as then "go'i" else showLA $ head as
+
+showLA (Just (Con (LA n))) = "la " ++ n
+
+onlyTop = filter (not . null . fst) .
+	map (\(vars, val) -> (filter isTop vars, val))
+
+isTop :: Term String s -> Bool
+isTop (Var "top" _) = True
+isTop _ = False
 
 data Atom
 	= LA String
@@ -31,10 +49,10 @@ data Atom
 
 type Scope = String
 
-readSumti :: Sumti -> Term Scope Atom
-readSumti (P.LA (_, "la", _) _ _ ns _) = Con $ LA $ concat $ map snd3 ns
-readSumti (P.LALE (_, "lo", _) _ st _ _) = Con $ LO $ readSumtiTail st
-readSumti (P.KOhA (_, k, _) _) = Var "" $ KOhA k
+readSumti :: Scope -> Sumti -> Term Scope Atom
+readSumti sc (P.LA (_, "la", _) _ _ ns _) = Con $ LA $ concat $ map snd3 ns
+readSumti sc (P.LALE (_, "lo", _) _ st _ _) = Con $ LO $ readSumtiTail st
+readSumti sc (P.KOhA (_, k, _) _) = Var sc $ KOhA k
 
 readSumtiTail :: SumtiTail -> String
 readSumtiTail (SelbriRelativeClauses (P.Brivla (_, n, _) _) _) = n
@@ -47,25 +65,27 @@ readSelbri (P.Brivla (_, n, _) _) = Left $ Con $ Brivla n
 readSelbri (P.GOhA (_, n, _) _ _) = Left $ Con $ GOhA n
 readSelbri (P.NA (_, "na", _) _ s) = Right $ Con $ readSelbriAtom s
 
-readSentenceFact :: Sentence -> Either (Fact Scope Atom) (Fact Scope Atom)
-readSentenceFact s@(TermsBridiTail _ _ _ _) =
+readSentenceFact :: Scope -> Sentence -> Either (Fact Scope Atom) (Fact Scope Atom)
+readSentenceFact sc s@(TermsBridiTail _ _ _ _) =
 	either (Left . (: (h ++ t))) (Right . (: (h ++ t))) f
 	where
-	h = map readSumti $ headTerms s
+	h = map (readSumti sc) $ headTerms s
 	f = readSelbri $ selbri $ bridiTail s
-	t = map readSumti $ tailTerms $ bridiTail s
+	t = map (readSumti sc) $ tailTerms $ bridiTail s
 
-readSentence :: Sentence -> Rule Scope Atom
-readSentence s@(TermsBridiTail _ _ _ _) = Rule (f : h ++ t) [] [] []
+readSentence :: Scope -> Sentence -> Rule Scope Atom
+readSentence sc s@(TermsBridiTail _ _ _ _) = Rule (f : h ++ t) [] [] []
 	where
-	h = map readSumti $ headTerms s
+	h = map (readSumti sc) $ headTerms s
 	Left f = readSelbri $ selbri $ bridiTail s
-	t = map readSumti $ tailTerms $ bridiTail s
-readSentence (IJoikJek s [r]) = Rule f [] (getRule r) (getNotRule r)
+	t = map (readSumti sc) $ tailTerms $ bridiTail s
+readSentence sc (IJoikJek s [r]) = Rule f [] (getRule sc r) (getNotRule sc r)
 	where
-	Left f = readSentenceFact s
+	Left f = readSentenceFact sc s
 
-getRule (_, Jek _ _ (_, "ja", _) (Just (_, "nai", _)), _, Just t) = lefts $ readTUhE t
-getNotRule (_, Jek _ _ (_, "ja", _) (Just (_, "nai", _)), _, Just t) = rights $ readTUhE t
+getRule sc (_, Jek _ _ (_, "ja", _) (Just (_, "nai", _)), _, Just t) =
+	lefts $ readTUhE sc t
+getNotRule sc (_, Jek _ _ (_, "ja", _) (Just (_, "nai", _)), _, Just t) =
+	rights $ readTUhE sc t
 
-readTUhE (TUhE _ _ _ t _ _) = map readSentenceFact $ getSentences t
+readTUhE sc (TUhE _ _ _ t _ _) = map (readSentenceFact sc) $ getSentences t
