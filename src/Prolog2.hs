765a08d836181f839d7ddff552453d38bccf4edb
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-09-21 16:58:13 +0900

    refactoring Prolog2.hs by -Wall and hlint

--- src/Prolog2.hs
+++ src/Prolog2.hs
@@ -13,17 +13,17 @@ import Data.Maybe
 ask :: (TwoD sc, Eq sc, Eq s) =>
 	sc -> Result sc s -> Fact sc s -> [Rule sc s] -> [Result sc s]
 ask sc ret q rs =
-	concat $ zipWith (\sc r -> askrule sc ret q r rs) (iterate next $ down sc) rs
+	concat $ zipWith (\sc' r -> askrule sc' ret q r rs) (iterate next $ down sc) rs
 
 askrule :: (TwoD sc, Eq sc, Eq s) =>
 	sc -> Result sc s -> Fact sc s -> Rule sc s -> [Rule sc s] -> [Result sc s]
-askrule sc ret q r@(Rule fact _ facts notFacts) rs =
-	filter (flip checkAll nots) ret'
+askrule sc ret q (Rule fact _ facts notFacts) rs =
+	filter (`checkAll` nots) ret'
 	where
-	ret' = foldl (\rets (sc', f) -> rets >>= \r -> ask sc' r f rs) r0 $ zip (iterate next sc0) $
-		map (const . ($ sc0)) facts
+	ret' = foldl (\rets (sc', f) -> rets >>= \r' -> ask sc' r' f rs) r0 $
+		zip (iterate next sc0) $ map (const . ($ sc0)) facts
 	sc0 = down sc
-	r0 = case (q sc) `unification` (fact sc0) of
+	r0 = case q sc `unification` fact sc0 of
 		Nothing -> []
 		Just r0' -> maybeToList $ ret `merge` r0'
-	nots = concat $ map ((flip (notAsk sc0) rs) . const . ($sc0)) notFacts
+	nots = concatMap (flip (notAsk sc0) rs . const . ($sc0)) notFacts
