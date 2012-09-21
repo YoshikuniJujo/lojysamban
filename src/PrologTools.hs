8a7fd732c04fa0b8f606f0b998e2cc5aa54e7032
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-09-21 16:54:30 +0900

    refactoring PrologTools.hs by -Wall and hlint

--- src/PrologTools.hs
+++ src/PrologTools.hs
@@ -31,21 +31,21 @@ data Rule sc s = Rule (Fact sc s) [Unify sc s] [Fact sc s] [NotFact sc s]
 
 notAsk :: (TwoD sc, Eq sc, Eq s) => sc ->
 	Fact sc s -> [Rule sc s] -> [Maybe [(Term sc s, Term sc s)]]
-notAsk sc q rs = zipWith (\sc r -> notAskRule sc q r rs) (iterate next $ down sc) rs
+notAsk sc q rs = zipWith (\sc' r -> notAskRule sc' q r rs) (iterate next $ down sc) rs
 
 notAskRule :: (TwoD sc, Eq sc, Eq s) => sc ->
 	Fact sc s -> Rule sc s -> [Rule sc s] -> Maybe [(Term sc s, Term sc s)]
-notAskRule sc q r@(Rule fact unify facts notFacts) rs = liftA concat $
+notAskRule sc q (Rule fact _ facts _) rs = liftA concat $
 	case match of
 		Nothing -> Just []
 		Just _ ->
-			liftA2 (:) start $ maybeOut $ concat $
-				map ((flip (notAsk sc) rs) . const . ($ sc)) facts
+			liftA2 (:) start $ maybeOut $ concatMap
+				(flip (notAsk sc) rs . const . ($ sc)) facts
 	where
 	start = notUnification (q sc) $ fact sc
 	match = unification [head $ q sc] [head $ fact sc]
 
 maybeOut :: [Maybe a] -> Maybe [a]
 maybeOut [] = Just []
-maybeOut (Nothing : xs) = Nothing
-maybeOut (Just x : xs) = maybe Nothing (Just . (x :)) $ maybeOut xs
+maybeOut (Nothing : _) = Nothing
+maybeOut (Just x : xs) = fmap (x :) $ maybeOut xs
